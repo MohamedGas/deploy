d9309432c5c0976e2f184bf87b3ab86f4ae04d8c
mohamed <ugaas62@hotmail.com>
2023-01-03 23:20:43 -0800

    controller changed

--- src/main/java/com/example/springdeployment/controller/HomeController.java
+++ src/main/java/com/example/springdeployment/controller/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public  String welcome(){
-        return "welcome to spring framework";
+        return "welcome to shiine";
     }
 }
